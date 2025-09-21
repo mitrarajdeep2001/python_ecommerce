fd0c19910d3bcf0235c70be9a0de7e105df579ab
Rajdeep <rajdeepmitra8@gmail.com>
2025-09-22 00:47:10 +0530

    update

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('products/', include('product.urls')),
 ]
 
 if settings.DEBUG:
---
+++ product/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'web/product_list.html', {'products': products})
